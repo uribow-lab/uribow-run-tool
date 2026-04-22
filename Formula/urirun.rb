b135c9e97637e1d946cee656b07cd0c8073720f9
uribow-lab <butsuyoku.taisho@gmail.com>
2026-04-22 10:02:36 +0900

    Bump Formula to v1.3.1

--- Formula/urirun.rb
+++ Formula/urirun.rb
@@ -1,10 +1,10 @@
 class Urirun < Formula
   desc "YAML設定に基づいてアプリやサービスをワンコマンドで起動するランチャー"
   homepage "https://github.com/uribow-lab/uribow-run-tool"
-  url "https://github.com/uribow-lab/uribow-run-tool/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "67920a9646d802c1d489300128a1b97d1f1258e661c450868477f9f09c17fb28"
+  url "https://github.com/uribow-lab/uribow-run-tool/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "65b29c7c27363bb290950458fb4838e8d7076f2d462a58c2ab90371ffaf11a68"
   license "MIT"
-  version "1.2.0"
+  version "1.3.1"
 
   def install
     bin.install "bin/urirun"
